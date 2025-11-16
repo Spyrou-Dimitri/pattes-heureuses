3fe514cd585474b6a0dc23eaae8b45e559489f85
Spyrou-Dimitri <dimitri.spyrou@student.hepl.be>
2025-11-16 13:28:27 +0100

    About => section cta-invitation finish

---
+++ pattes-heureuses/lang/fr/client/about/cta-invitation/cta-invitation.php
@@ -0,0 +1,10 @@
+<?php
+
+
+return [
+    'title' => 'Devenir bénévole !',
+    'content' => 'Rejoignez l’aventure des Pattes Heureuses et participez à rendre chaque jour un peu plus beau pour nos pensionnaires.  Que ce soit pour promener les chiens, câliner les chats ou prêter main-forte au refuge, chaque geste compte.  Si vous souhaitez devenir bénévole, contactez-nous dès aujourd’hui — vos mains peuvent faire la différence !',
+    'cta' => "Devenir bénévole",
+    'cta-title' => 'Accéder à la page des contact',
+    'alt' => "Un chat en costume"
+];
--- pattes-heureuses/resources/views/client/about.blade.php
+++ pattes-heureuses/resources/views/client/about.blade.php
@@ -1,4 +1,5 @@
 <x-layouts.auth>
     <x-client.sections.about.section-landing></x-client.sections.about.section-landing>
     <x-client.sections.about.section-story></x-client.sections.about.section-story>
+    <x-client.sections.about.section-cta-invitation></x-client.sections.about.section-cta-invitation>
 </x-layouts.auth>
