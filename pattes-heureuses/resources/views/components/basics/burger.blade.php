060581ca8369d86d2d26cc104fe17d4f74eb297d
Spyrou-Dimitri <dimitri.spyrou@student.hepl.be>
2025-11-22 17:32:43 +0100

    Fix bug burger menu

--- pattes-heureuses/resources/views/components/basics/burger.blade.php
+++ pattes-heureuses/resources/views/components/basics/burger.blade.php
@@ -1,8 +1,8 @@
 <input type="checkbox" name="burger" id="burger__button">
 <label for="burger__button" class="sr-only">{{__('client/nav.burger-menu')}}</label>
 <div class="burger__wrapper">
-    <span class="burger__wrapper__lines"></span>
-    <span class="burger__wrapper__lines"></span>
-    <span class="burger__wrapper__lines"></span>
+    <span class="burger__wrapper__lines up"></span>
+    <span class="burger__wrapper__lines middle"></span>
+    <span class="burger__wrapper__lines down"></span>
 </div>
 
