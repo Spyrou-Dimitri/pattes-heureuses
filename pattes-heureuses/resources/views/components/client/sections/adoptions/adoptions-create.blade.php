be0b225307a0367ebf6585f3b7ce83e0bb3e425a
Spyrou-Dimitri <dimitri.spyrou@student.hepl.be>
2025-12-22 01:13:15 +0100

    Create component for adoptions form
    Needed to create a component cause i want to create some adoption from admin interface ( if someone come to the shelter and asked to adopt an animal )

--- pattes-heureuses/resources/views/components/client/sections/adoptions/adoptions-create.blade.php
+++ pattes-heureuses/resources/views/components/client/sections/adoptions/adoptions-create.blade.php
@@ -10,85 +10,8 @@
                 Remplissez ce formulaire avec soin afin que nous puissions organiser la rencontre et finaliser
                 l’adoption dans les meilleures conditions.
             </p>
+            <x-forms.form.adoption-form :animal_id="$animal->id" />
 
-            <form class="flex flex-col gap-8 p-6 border border-main-blue rounded-lg bg-white" action="{{route('adoption.store')}}" method="POST">
-                @csrf
-
-                <input name="animal_id" type="hidden" label="Animal" value="{{$animal->id}}">
-
-                <fieldset class="flex flex-col gap-4">
-                    <legend>
-                        Informations personnelles
-                    </legend>
-                    <div class="flex flex-col gap-4 border-t-2 border-t-main-blue pt-5 lg:flex-row lg:justify-between">
-                        <x-forms.input :name="'last_name'"
-                                       :type="'text'"
-                                       :label="'Nom'"
-                                       :placeholder="'Doe'"
-                                       :required="true">
-                        </x-forms.input>
-                        <x-forms.input :name="'first_name'"
-                                       :type="'text'"
-                                       :label="'Prénom'"
-                                       :placeholder="'John'"
-                                       :required="true">
-
-                        </x-forms.input>
-                    </div>
-                    <div class="flex flex-col gap-4 lg:flex-row lg:justify-between">
-                        <x-forms.input :name="'email'"
-                                       :type="'email'"
-                                       :label="'Email'"
-                                       :placeholder="'john.doe@gmail.com'"
-                        :required="true">
-
-                        </x-forms.input>
-                        <x-forms.input :name="'telephone'"
-                                       :type="'tel'"
-                                       :label="'Téléphone'"
-                                       :placeholder="'0485 48 48 30'">
-
-                        </x-forms.input>
-                    </div>
-                </fieldset>
-                <fieldset class="flex flex-col gap-4">
-                    <legend>
-                        Informations sur votre logement
-                    </legend>
-                    <div class="flex flex-col gap-4 border-t-2 border-t-main-blue pt-5 lg:flex-row lg:justify-between">
-                        <x-forms.input :name="'housing_type'"
-                                       :type="'text'"
-                                       :label="'Type de logement'"
-                                       :placeholder="'Appartement'">
-
-                        </x-forms.input>
-                        <x-forms.input :name="'environment'"
-                                       :type="'text'"
-                                       :label="'Environnement'"
-                                       :placeholder="'Jardin / forêt'">
-
-                        </x-forms.input>
-                    </div>
-                </fieldset>
-                <fieldset class="flex flex-col gap-4">
-                    <legend>
-                        Expériences et motivations
-                    </legend>
-                    <div class="flex flex-col gap-4 border-t-2 border-t-main-blue pt-5">
-                        <x-forms.textarea
-                            :name="'motivations'"
-                            :label="'Pourquoi souhaitez-vous adopter ?'"
-                            :placeholder="'Je souhaite adopté Jean parce que je veux pouvoir lui donner un nouveaux foyer...'">
-
-                        </x-forms.textarea>
-                    </div>
-
-                </fieldset>
-                <x-forms.submit>
-                    Envoyer
-                </x-forms.submit>
-
-            </form>
         </div>
         <x-cards.animal-data class="lg:col-span-5 lg:sticky lg:top-20 lg:right-0"
                              :name="$animal->name"
