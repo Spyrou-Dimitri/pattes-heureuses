6b6547b38c567ce9bfb3ad6cfb78aa2a1bcd30eb
Spyrou-Dimitri <dimitri.spyrou@student.hepl.be>
2025-12-27 03:32:03 +0100

    Correct bug of modal filter

--- pattes-heureuses/resources/views/pages/animals/⚡index.blade.php
+++ pattes-heureuses/resources/views/pages/animals/⚡index.blade.php
@@ -49,6 +49,7 @@ new class extends Component {
 
     public function applyFilters()
     {
+        $this->dispatch('close-modal');
     }
 
 
@@ -180,7 +181,9 @@ new class extends Component {
                 @endforeach
             </ul>
 
-            <div class="flex flex-col justify-between sm:flex-row gap-4 md:justify-start" x-data="{open: false}">
+            <div class="flex flex-col justify-between sm:flex-row gap-4 md:justify-start"
+                 x-data="{open: false}"
+                 @close-modal.window="open = false">
                 <x-forms.input :type="'search'" :name="'animal-search'"
                                :label="__('client/animals/index/landing.search-bar-label')"
                                :placeholder="__('admin/animals/index.search_bar_placeholder')"/>
