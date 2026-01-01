f1493d889d8b84ad995b87d5c857c94c2260a884
Spyrou-Dimitri <dimitri.spyrou@student.hepl.be>
2026-01-01 17:53:14 +0100

    fix bug for production

--- database/migrations/2025_12_04_123910_create_breeds_table.php
+++ database/migrations/2025_12_04_123910_create_breeds_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
         Schema::create('breeds', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('specie_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('specie_id')->constrained('species')->cascadeOnDelete();
             $table->timestamps();
         });
     }
