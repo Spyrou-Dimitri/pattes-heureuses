b979a60f104d6a2d2c089a771bff6df2a0f39f77
Spyrou-Dimitri <dimitri.spyrou@student.hepl.be>
2026-01-01 18:20:50 +0100

    Change seeder for a test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,31 +29,21 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        Message::factory(50)->create();
-        Message::create([
-            'last_name' => 'Dupont',
-            'first_name' => 'Jean',
-            'email' => 'jean.dupont@example.com',
-            'telephone' => '+32470123456',
-            'topic' => 'Test message',
-            'description' => 'Ceci est un message de test créé pour 2023.',
-            'is_read' => false,
-            'created_at' => Carbon::create(2023, 5, 15, 14, 30, 0),
-            'updated_at' => Carbon::create(2023, 5, 15, 14, 30, 0),
-        ]);
+
 
         /* User seeding */
-        User::factory(10)->create();
         User::factory()->create([
-            'first_name' => 'Admin',
+            'first_name' => 'Elise',
             'last_name' => 'Dieu',
             'email' => 'admin@example.com',
+            'telephone' => '+32 036 27 20 17',
             'role' => RoleVolunteer::Admin,
         ]);
         User::factory()->create([
-            'first_name' => 'Bénévole',
+            'first_name' => 'Thomas',
             'last_name' => 'Gueux',
             'email' => 'test@example.com',
+            'telephone' => '+32 036 27 20 17',
             'role' => RoleVolunteer::Volunteer,
         ]);
 
@@ -143,50 +133,5 @@ class DatabaseSeeder extends Seeder
         foreach ($behaviors as $behavior) {
             $behavior = Behavior::create(['name' => $behavior]);
         }
-
-        $allBehaviors = Behavior::all();
-        /* Seed des animaux */
-        for ($i = 0; $i < 50; $i++) {
-            $animals = Animal::factory()->create([
-                'sexe' => SexeAnimal::cases()[array_rand(SexeAnimal::cases())]->value,
-                'breed_id' => $seedingBreeds[array_rand($seedingBreeds)],
-                'created_at' => Carbon::now()->subMonths(rand(0, 12)),
-                'updated_at' => function (array $attrs) {
-                    return Carbon::parse($attrs['created_at']);
-                },
-
-
-            ]);
-
-            $animals->coats()->attach(
-                $allCoats->random(rand(1, 2))->pluck('id')->toArray()
-            );
-
-            $animals->behaviors()->attach(
-                $allBehaviors->random(rand(1,3))->pluck('id')->toArray()
-            );
-
-        }
-        $animals = Animal::all();
-
-        $animalsToAdopt = $animals->random((int) ($animals->count() * 0.6));
-
-        foreach ($animalsToAdopt as $animal) {
-
-            $adoptionDate = Carbon::parse($animal->created_at)
-                ->addDays(rand(1, 30));
-
-            Adoption::create([
-                'animal_id' => $animal->id,
-                'first_name' => fake()->firstName(),
-                'last_name' => fake()->lastName(),
-                'email' => fake()->safeEmail(),
-                'telephone' => rand(0, 1) ? fake()->phoneNumber() : null,
-                'status' => AdoptionStatus::cases()[array_rand(AdoptionStatus::cases())]->value,
-                'created_at' => $adoptionDate,
-                'updated_at' => $adoptionDate,
-            ]);
-        }
-
     }
 }
